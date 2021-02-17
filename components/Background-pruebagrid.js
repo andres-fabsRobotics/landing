95729f53471a5da951acef22118ed69ced58a296
andresbr92 <andresbr92@gmail.com>
2021-02-17 09:33:27 +0100

    new landing

--- components/Background-pruebagrid.js
+++ components/Background-pruebagrid.js
@@ -2,6 +2,8 @@ class Hexagon extends HTMLElement {
   constructor(){
     super();
     let shadow = this.attachShadow({mode: 'open'});
+    this.hoverActive = true;
+    this.hola = "hola";
     shadow.innerHTML = `
     <style>
       :host{
@@ -24,7 +26,7 @@ class Hexagon extends HTMLElement {
         border-radius: 50%;
         transition: all 0.6s ease;
         opacity:0;
-        background: radial-gradient(circle, rgba(236, 182, 84,1) 50%, rgba(236, 182, 84,0) 75%)
+        background: radial-gradient(circle, rgba(100,100,90, 1) 50%, rgba(100,100,90, 0) 75%)
       }
     </style>
     <div id="background"></div>
@@ -34,13 +36,6 @@ class Hexagon extends HTMLElement {
     `;
   }
   connectedCallback(){
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-      // true for mobile device
-      console.log("mobile device");
-    }else{
-      // false for not mobile device
-      console.log("not mobile device");
-    }
     const width = window.getComputedStyle(this).getPropertyValue('width');
     const height = window.getComputedStyle(this).getPropertyValue('height');
     const style = document.createElement('style');
@@ -50,10 +45,11 @@ class Hexagon extends HTMLElement {
         left: calc( ( ${width} - ( 3 * ${height} / 2 ) ) / 2 );
       }
     `;
-    this.shadowRoot.appendChild(style);
-    this.setPath(width,height);
-    this.drawGradient();
+    this.shadowRoot.appendChild(style)
+    this.setPath(width,height)
+    this.drawGradient()
     this.clickOnElement()
+    this.isMobile()
   }
   setPath(width,height){
     width = parseFloat(width.substring(0, width.length - 2));
@@ -74,13 +70,13 @@ class Hexagon extends HTMLElement {
 
    const hex = this.shadowRoot.getElementById('container')
    const circleBackground = this.shadowRoot.getElementById('background')
-   hex.addEventListener('mouseover', e => {
+   if(this.hoverActive) hex.addEventListener('mouseover', e => {
      circleBackground.style = `
      opacity:1;
    `
    })
    hex.addEventListener('mouseout', e => {
-    circleBackground.style = `
+    if(this.hoverActive) circleBackground.style = `
     opacity:0;
    `
   })
@@ -89,14 +85,28 @@ class Hexagon extends HTMLElement {
   clickOnElement() {
     const hex = this.shadowRoot.getElementById('container')
     hex.addEventListener('click', e => {
-      console.log(e)
+      console.log(e.className)
     })
 
   }
 
-
-  
-  
+  isMobile(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      // true for mobile device
+      const hex = this.shadowRoot.getElementById('container')
+      console.log(hex)
+    }else{
+      // false for not mobile device
+      console.log("not mobile device");
+    }
+  }
+  sayHellow(){
+    console.log("hokla");
+  }
+  set hover(value){
+    console.log("lo cambio");
+    this.hoverActive = value;
+  }
 }
 
 export default class Backgrid extends HTMLElement {
@@ -111,7 +121,7 @@ export default class Backgrid extends HTMLElement {
       #container{
         width: 100%;
         height: 100%;
-        background-color: grey;
+        background-color: white ;
       }
     </style>
     <div id="container"></div>
@@ -133,11 +143,9 @@ export default class Backgrid extends HTMLElement {
      //resizing
      const computeWidth = parseFloat(width.substring(0, width.length - 2))
      const computeHeight = parseFloat(height.substring(0, height.length - 2))
-     console.log(computeWidth, computeHeight);
-     let hexH = parseFloat(height.substring(0, height.length - 2))/7;
-     if(computeWidth > 1300) hexH = parseFloat(height.substring(0, height.length - 2))/4;
-     if(computeWidth < 1300 && computeWidth > 1000) hexH = parseFloat(height.substring(0, height.length - 2))/5;
-    
+     
+  
+     let hexH = 200
      
     
     let hexW = hexH*Math.cos(Math.PI/6); 
@@ -145,7 +153,7 @@ export default class Backgrid extends HTMLElement {
     let side = hexH/2;
     let apothem = hexW/2;
     let wmargin = 10;
-    let hmargin = wmargin*Math.tan(Math.PI/3)/2; 
+    let hmargin = wmargin*Math.tan(Math.PI/3)/2;
 
     width = parseFloat(width.substring(0, width.length - 2))+hexW;
     height = parseFloat(height.substring(0, height.length - 2))+hexH;
@@ -158,46 +166,29 @@ export default class Backgrid extends HTMLElement {
     //let widthN = parseInt((4*width)/(9*apothem));
     let heightN = parseInt((height-(2*side))/((3*side/2) + hmargin));
     let widthN = parseInt((width-apothem)/(2*apothem + wmargin));
+    console.log(heightN, widthN);
 
     // draw Hexes
-    for (let j = 0; j < heightN; j++) {
-      for (let i = 0; i < widthN; i++) {
+    for (let j = 0; j < heightN+2; j++) {
+      for (let i = 0; i < widthN+2; i++) {
         //if(i == widthN+1 && j%2 == 1) break;
         //if(j == 0 && i == 0) break;
         if(j == 0 && i == 0) {
-          null
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"white");
         } else if (j == 0 && i == 1) {
-          null
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"white");
         }else if (j == 1 && i == 0) {
-          null
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"white");
         } else if (j == 2 && i == 0) {
-          null
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"white");
         } else if (j == 0 && i == 2) {
-          null
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"white");
         } else if (j == 1 && i == 1) {
-          null
-        } else if (j == 2 && i == 1) {
-          null
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"white");
+        } else if (j >= heightN) {
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"white");
         } else  {
-          let hex = document.createElement('fabs-hexagon');
-          let content = document.createElement('div');
-          content.style = `
-            width: 100%;
-            height: 100%;
-            background-color: rgba(255,255,255,1);
-            z-index: 2;
-           
-          `
-          content.className = `hex${i}${j}`
-          hex.style = `
-            position: absolute;
-            top: ${(j*3*side/2)+j*hmargin-side}px;
-            left: ${(i*2*apothem)+(j%2)*apothem+i*wmargin-apothem+(j%2)*(wmargin/2)}px;
-            width: ${hexW}px;
-            height: ${hexH}px;
-          `;
-          hex.appendChild(content);
-          this.shadowRoot.getElementById('container').appendChild(hex);
+          this.drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,"rgb(237,183,85)");
         }
      
         
@@ -205,9 +196,34 @@ export default class Backgrid extends HTMLElement {
     }
   }
 
+  drawHex(j,i,hexH,hexW,apothem,wmargin,hmargin,side,color){
+    let hex = document.createElement('fabs-hexagon');
+    let content = document.createElement('div');
+    content.style = `
+      width: 100%;
+      height: 100%;
+      background-color: ${color};
+      z-index: 2;
+     
+    `
+    content.className = `hex${i}${j}`
+    hex.style = `
+      position: absolute;
+      top: ${(j*3*side/2)+j*hmargin-side}px;
+      left: ${(i*2*apothem)+(j%2)*apothem+i*wmargin-apothem+(j%2)*(wmargin/2)}px;
+      width: ${hexW}px;
+      height: ${hexH}px;
+    `;
+    hex.appendChild(content);
+    if(color == "white") {
+      console.log(hex.hola);
+      hex.hover = false;
+    }
+    this.shadowRoot.getElementById('container').appendChild(hex);
+  }
  
 
 }
 
-window.customElements.define("back-grid", Backgrid);
-window.customElements.define("fabs-hexagon", Hexagon);
+customElements.define("back-grid", Backgrid);
+customElements.define("fabs-hexagon", Hexagon);
